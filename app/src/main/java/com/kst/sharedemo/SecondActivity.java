43b7f17c62f1cb606dcbf635ef579175e71afae1
DOUjunjun <952171979@qq.com>
2015-04-28 10:16:04 +0800

    dou

--- app/src/main/java/com/kst/sharedemo/SecondActivity.java
+++ app/src/main/java/com/kst/sharedemo/SecondActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+/**
+ * aaaaaaaaaa
+ */
 public class SecondActivity extends ActionBarActivity {
 
     @Override
